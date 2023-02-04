274e81e975a0d175ebe90c390cdf41b0ef2b99a0
chonchanat <t.chonchanat@hotmail.com>
2023-02-04 15:14:15 +0700

    update: privateRoute

--- src/privateRoute/index.js
+++ src/privateRoute/index.js
@@ -1,12 +1,29 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
+
+import Cookies from 'js-cookie';
+import { useEffect } from "react";
+import { setAuth } from "../actions/authActions";
 
 function PrivateRoute({ children }) {
 
+    const dispatch = useDispatch();
     const reducers = useSelector(state => state);
     // console.log(reducers);
+    const userCookie = Cookies.get("userCookie");
 
-    return reducers.authReducer ? children : <Navigate to="/customer-signin"/>;
+    useEffect(() => {
+        if (userCookie) {
+            dispatch(setAuth(JSON.parse(userCookie)));
+        }
+    }, [userCookie, dispatch]);
+
+    if (reducers.authReducer) {
+        return userCookie !== undefined ? children : <Navigate to="/customer-signin" />;
+    }
+    if (reducers.authReducer === null && userCookie === undefined) {
+        return <Navigate to="/customer-signin" />;
+    }
 }
 
 export default PrivateRoute;
