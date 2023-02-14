4b61545f5efcb6dcfbc04d949b3cc4d38562ad1c
chonchanat <t.chonchanat@hotmail.com>
2023-02-14 14:47:25 +0700

    build: leaflet setup and builid simple map

--- src/pages/CustomerMap.js
+++ src/pages/CustomerMap.js
@@ -2,13 +2,62 @@ import { Navbar } from '../components/Navbar';
 import { BlockMobile } from '../components/Block'
 import { CardWithHead } from '../components/Card';
 
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 function CustomerMap() {
+
+    const camelPosition = [12.8235, 99.9388];
+
+    const activityPosition = [
+        {
+            name: "คาปิบาร่า",
+            pos: [12.8238, 99.9384],
+            text: "ให้อาหารคาปิบาร่า",
+        },
+        {
+            name: "อูฐ",
+            pos: [12.8240, 99.9393],
+            text: "ขี่อูฐ",
+        },
+        {
+            name: "คาร์ทไรเดอร์",
+            pos: [12.8242, 99.9397],
+            text: "ขับคาร์ทไรเดอร์ตะลอน",
+        },
+        {
+            name: "เครื่องซักผ้า",
+            pos: [12.8235, 99.9395],
+            text: "ปั่นให้คุณมึนตึ๊บ",
+        },
+    ]
+
     return (
         <div>
             <Navbar />
             <BlockMobile>
                 <CardWithHead title="แผนที่" bgColor="#F8F8F8">
-
+                    <div className="w-full bg-red-300">
+                        <MapContainer center={camelPosition} zoom={17} scrollWheelZoom={true} style={{ height: "500px" }}>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            {activityPosition.map((actPos) =>
+                                <Marker position={actPos.pos}>
+                                    <Popup>
+                                    <p className="text-center font-bold">{actPos.name}</p>
+                                    <p>{actPos.text}</p>
+                                    </Popup>
+                                </Marker>
+                            )}
+                        </MapContainer>
+                    </div>
                 </CardWithHead>
             </BlockMobile>
         </div>
