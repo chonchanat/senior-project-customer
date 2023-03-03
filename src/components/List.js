49883ff729ef5e931f8c5f65537392c20a47e76b
chonchanat <t.chonchanat@hotmail.com>
2023-03-03 16:24:00 +0700

    update: ListComment for status done,cancel

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import { AiFillStar, AiOutlineStar,AiOutlineControl } from 'react-icons/ai';
+import { AiFillStar, AiOutlineStar, AiOutlineControl } from 'react-icons/ai';
 
 function StarRating({ rating }) {
     return (
@@ -15,12 +15,12 @@ function StarRating({ rating }) {
     );
 }
 
-function ListAllAcitivty({ data, click=()=>{return 0}, control, role }) {
+function ListAllAcitivty({ data, click = () => { return 0 }, control, role }) {
 
     const diffRound = data.round - data.roundNow;
 
     function handlerStatus() {
-        if ( data.status === "open") return "bg-accept";
+        if (data.status === "open") return "bg-accept";
         else if (data.status === "temporarily closed") return "bg-yellow";
         else if (data.status === "close") return "bg-red-400";
     }
@@ -29,12 +29,12 @@ function ListAllAcitivty({ data, click=()=>{return 0}, control, role }) {
         <div className="py-2 flex h-fit hover:bg-hover" key={data._id} onClick={() => click(data)}>
             <div className="h-auto w-[30%] flex items-center relative">
                 <img src={data.picture} className="h-[80px] w-[120px] rounded-md" alt="iamge of activity" />
-                <div className={`${handlerStatus()} w-4 h-4 absolute rounded-full right-[-8px] top-0`}/>
+                <div className={`${handlerStatus()} w-4 h-4 absolute rounded-full right-[-8px] top-0`} />
             </div>
             <div className="w-[70%] pl-4">
                 <div className="flex justify-between items-center">
                     <p className="font-bold">{data.name[0]}</p>
-                    {role !== "customer" && data.code === control &&<AiOutlineControl className="text-yellow text-2xl"/>}
+                    {role !== "customer" && data.code === control && <AiOutlineControl className="text-yellow text-2xl" />}
                 </div>
                 <StarRating rating={data.rating} />
                 <div className="flex justify-between mt-4">
@@ -56,7 +56,7 @@ function ListAllAcitivty({ data, click=()=>{return 0}, control, role }) {
     );
 }
 
-function ListQueue({ data, click=()=>{return 0} }) {
+function ListQueue({ data, click = () => { return 0 } }) {
     return (
         <div className="py-2 flex h-fit hover:bg-hover" key={data._id} onClick={() => click(data)}>
             <div className="h-auto w-[30%] overflow-hidden flex items-center">
@@ -85,15 +85,23 @@ function ListQueue({ data, click=()=>{return 0} }) {
 }
 
 function ListComment({ data, setSelectData }) {
+    function comment() {
+        if (data.status === "done") setSelectData(data);
+    }
     return (
-        <div className="py-2 flex h-fit hover:bg-hover" key={data.id} onClick={() => setSelectData(data)}>
+        <div className="py-2 flex h-fit hover:bg-hover" key={data.id} onClick={comment}>
             <div className="h-auto w-[30%] overflow-hidden flex items-center">
-                <img src={data.image} alt="iamge of activity" />
+                <img src={data.image} className="h-[80px] w-[120px] rounded-md" alt="iamge of activity" />
             </div>
             <div className="flex flex-col w-[70%] pl-4">
                 <p className="font-bold">{data.name[0]}</p>
                 <p className="text-sm flex items-center">ผู้เข้าร่วมกิจกรรม : 4 คน ( 48 <AiFillStar className="text-yellow text-sm" />)</p>
-                <p className="text-xs flex-1 flex justify-end items-end">กดเพื่อให้คะแนน</p>
+                <p className="text-sm flex items-center">สถานะ :
+                    <span className={`${data.status === "done" ? "text-accept" : "text-decline"} ml-1`}>
+                        {data.status === "done" ? "เข้าร่วมแล้ว" : "ยกเลิกการจอง"}
+                    </span>
+                </p>
+                {data.status === "done" && <p className="text-xs flex-1 flex justify-end items-end">กดเพื่อให้คะแนน</p>}
             </div>
         </div>
     );
